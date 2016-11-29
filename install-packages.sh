e761a5614865578b9eac0776ce8aa15b72730e7e
Chris Collins <collins.christopher@gmail.com>
2016-11-29 14:23:31 -0500

    adding new package & script installer feature

--- install-packages.sh
+++ install-packages.sh
@@ -2,47 +2,82 @@
 
 log () {
     message="$1"
-    local logfile="/var/log/stevedore-packages.log"
+    local logfile="/var/log/custom_setup.log"
     if [[ ! -f  ${logfile} ]]
-        then touch ${logfile}
+    then 
+	touch ${logfile}
         chmod a+r ${logfile}
     fi
 
-    echo "$(date '+%Y-%m-%dT%H:%M') - ${message}" >> $logfile
+    echo "$(date '+%Y-%m-%dT%H:%M') - ${message}" | tee $logfile
 }
 
 
 main () {
     local appdir="${1}"
-    local ubuntu_packages="${appdir}/.stevedore/ubuntu_packages"
-    local r_packages="${appdir}/.stevedore/r_packages"
+
+    log "[INFO] Searching ${appdir} for custom setup files"
+
+    local ubuntu_packages="${appdir}/ubuntu_packages"
+    local r_packages="${appdir}/r_packages"
+    local r_repos="${appdir}/r_repos"
+    local r_script="${appdir}/r_config.R"
+
+    local repos=()
+    repos+=("${R_REPOS}")
 
     unset packages
 
     if [[ -f ${ubuntu_packages} ]]
-        then declare -a packages
-        log "Found ${ubuntu_packates}"
+    then 
+	declare -a packages
+        log "[INFO] Found ${ubuntu_packages}"
         for package in $(cat ${ubuntu_packages})
-            do log "Adding ${package} to install job"
+        do
+            log "[INFO] Adding ${package} to install job"
             packages+=(${package})
       done
       if  [[ ${#packages[@]} != 0 ]]
-          then apt-get update | tee "/var/log/stevedore-packages.log" \
+      then
+	  apt-get update \
+	  | tee "/var/log/custom_setup.log" \
           || exit 1
-          apt-get install -y ${packages[@]} | tee "/var/log/stevedore-packages.log" \
+          apt-get install -y ${packages[@]} \
+	  | tee "/var/log/custom_setup.log" \
           || exit 1 
       fi
+    fi
+
+    if [[ -f ${r_repos} ]]
+    then
+        log "[INFO] Found ${r_repos}"
+	for repo in $(cat ${r_repos})
+        do
+	  log "[INFO] Adding ${repo} to R Repo list"
+	  repos+=(${repo})
+	done
+    fi
+
+    if [[ -f ${r_script} ]]
+    then
+	log "[INFO] Found ${r_script}"
+	R "${r_script}" \
+	| tee "/var/log/custom_setup.log" \
+	|| exit 1
     fi
 
     unset packages
 
     if [[ -f ${r_packages} ]]
-        then 
-        log "Found ${r_packages}"
+    then 
+        log "[INFO] Found ${r_packages}"
+        log "[INFO] Using R repos: ${repos[@]}"
         for package in $(cat ${r_packages})
         do 
-            log "Installing ${package} "
-            R -e "install.packages(c('${package}'),repos=${R_REPOS})" 
+            log "[INFO] Installing ${package} "
+            R -e "install.packages(c('${package}'),repos=${repos[@]})"  \
+	    | tee "/var/log/custom_setup.log" \
+	    || exit 1
         done
     fi
 }
